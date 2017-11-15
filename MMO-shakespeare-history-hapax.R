239a55958bd41e35bcfeffab6a1d33623f40fd8d
cmohge1 <christopherohge@gmail.com>
2017-11-15 14:11:18 +0000

    fixed hapax scripts to include the missing for loop

--- MMO-shakespeare-history-hapax.R
+++ MMO-shakespeare-history-hapax.R
@@ -15,10 +15,33 @@ divs.history.l <- list()
 div.freqs.l <- list()
 div.raws.l <- list()
 #the for loop will tokenize and calculate words in each div
+for(i in 1:length(divs.ns.l)){
+  #specifies div content for retrieval
+  div.content <- xmlValue(divs.ns.l[[i]], "div")[[1]]
+  #specifies word tag content for retrieval
+  words.ns <- xmlElementsByTagName(divs.ns.l[[i]], "w", recursive = TRUE)
+  #combines word tag content within each div
+  div.words.v <- paste(sapply(words.ns, xmlValue))
+  #convert characters to lowercase
+  words.lower.v <- tolower(div.words.v)
+  #tokenize words, restricting content to alphabetical characters,
+  #apostrophes (for contracted word forms), and hyphens (for compound expressions)
+  words.l <- strsplit(words.lower.v, "[^a-z'-]")
+  #strsplit product is a list. restore to vector with unlist
+  word.v <- unlist(words.l)
+  #remove any remaining whitespace created from culled punctuation
+  word.v <- word.v[which(word.v!="")]
+  #create a list of the relevant nodes and their word content
+  divs.tragedy.l[[div.content]] <- word.v
+  #calculate frequencies
+  div.freqs.t <- table(word.v)
+  div.raws.l[[div.content]] <- div.freqs.t
+  div.freqs.l[[div.content]] <- 100*(div.freqs.t/sum(div.freqs.t))
+}
 #for output varieties
 
 #outputs sequential node list and word tokens for the full object
-get.MMO.words(divs.history.l)
+(divs.history.l)
 divs.history.l
 #outputs nodes and word tokens for the full object, with raw word counts in nodes
 div.raws.l
